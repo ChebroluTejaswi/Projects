eca7d9cd3af635986a09ce0b957409190e849021
ChebroluTejaswi <tejaswichebrolu@gmail.com>
2022-06-27 23:14:30 +0530

    web template done

--- app.js
+++ app.js
@@ -12,11 +12,31 @@ app.set('view engine', 'ejs');
 app.get("/",function(req,res){
     res.render("home");
 });
+// -----------------------------------
+
+python={1:{"name": 'Tejaswi',"description": 'abcd',"github":'https://github.com/ChebroluTejaswi'}}
+app.get("/python",function(req,res){
+    res.render("python");
+});
+app.get("/python/:projectID", function(req, res){
+    res.render("project");
+});
+app.get("/abc",function(req,res){
+    res.render("project");
+});
+
+// --------------------------------
 app.get("/webdev",function(req,res){
     res.render("webdev");
 });
 app.get("/ml",function(req,res){
     res.render("ml");
+});
+app.get("/achievements",function(req,res){
+    res.render("achievement");
+});
+app.get("/onlinecertificates",function(req,res){
+    res.render("onlinecertificates");
 });
 // -----------------------------------
 app.listen(3000, function () {
